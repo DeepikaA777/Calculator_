e427691a409e8dd8263814e8c9ecd874539d86c3
Deepika A <deepika20060208@gmail.com>
2025-10-08 17:44:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let display = document.getElementById('display');
+function appendToDisplay(value) {
+    display.value += value;
+}
+function clearDisplay() {
+    display.value = '';
+}   
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
+function deleteLastCharacter() {
+    display.value = display.value.slice(0, -1);
+}
+function squareRoot() {
+    if (display.value) {
+    display.value = Math.sqrt(eval(display.value));
+  }
+}
